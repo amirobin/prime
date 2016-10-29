7a8a3b6d43199e041072ffc27688d216db03c4d8
Issac Robin <Issac Robin>
2016-10-29 05:54:06 +0300

    function to find the prime numbers

---
+++ PRIME.py
@@ -0,0 +1,27 @@
+#def is_prime(num):
+	#if num < 2 :
+	#	return False
+	#elif num == 2 or num == 3:
+	#	return True
+	#elif num % 2==0:
+	#	return False
+    #	for x in range(3,num):
+    #		if num % x == 0:
+    #			return False
+   	#	else:
+   	#		return True
+#print is_prime(35)
+
+def is_prime(num):
+	#for num in range(2,num):
+	#	prime =True;
+		for i in range(2,num):
+			if num%i==0:
+				prime=False;
+
+			else: 
+				prime=True;
+		if prime:
+			print num
+
+is_prime(20);
