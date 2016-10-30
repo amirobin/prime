47b3a83dd64f7794fbd286eb2604814839a965b1
amirobin <amirobin259@gmail.com>
2016-10-30 17:14:35 +0300

    Update PRIME.py
    Removed extra lines of code

--- PRIME.py
+++ PRIME.py
@@ -1,17 +1,3 @@
-#def is_prime(num):
-	#if num < 2 :
-	#	return False
-	#elif num == 2 or num == 3:
-	#	return True
-	#elif num % 2==0:
-	#	return False
-    #	for x in range(3,num):
-    #		if num % x == 0:
-    #			return False
-   	#	else:
-   	#		return True
-#print is_prime(35)
-
 def is_prime(num):
 	for num in range(2,num):
 		prime =True;
@@ -24,5 +10,3 @@ def is_prime(num):
 			print (num)
 
 is_prime(20);
-#editing
-#more editing
